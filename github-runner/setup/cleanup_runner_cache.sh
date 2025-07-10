4bcbe9102516ecaf6d9875ed10a02022d34051e8
Wei18 <11254896+Wei18@users.noreply.github.com>
2025-07-10 10:24:29 +0800

    [tmp] clean x64

--- github-runner/setup/cleanup_runner_cache.sh
+++ github-runner/setup/cleanup_runner_cache.sh
@@ -64,7 +64,7 @@ function is_monitored_runner {
             return 0
         fi
     done
-    if [[ "$name" == "APP-x86-Auto" ]]; then
+    if [[ "$name" == "Pinkoi-App" ]]; then
         return 0
     fi
     return 1
